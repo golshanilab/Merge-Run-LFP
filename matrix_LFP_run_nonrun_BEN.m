17d04d72d50b4986f0accd1c91d9c99d12c9fc0e
bamendol <ben.amendolara@gmail.com>
2017-03-02 10:11:58 -0800

    Update matrix_LFP_run_nonrun_BEN.m
    - made code more general for non-Ben users
    - improved comments in code

--- matrix_LFP_run_nonrun_BEN.m
+++ matrix_LFP_run_nonrun_BEN.m
@@ -4,35 +4,20 @@ Fs = 1000; %sampling rate = 1000 Hz
 numchans=128;
 numshanks=2;
 load('E:\Tristan\VLSE neuro 4-4\probe_data\probe128A.mat')
+cd(strcat(exp_dir, '\LFP\LFP1000\')); %change directory to experimental directory; e.g., 'E:\BenA\SLAYTraining\SLAY1-3\10-27-16\cortex'
 
-if animal == 'SLAY1-3'
-    exp_dir = 'E:\BenA\SLAYTraining\SLAY1-3\10-27-16\cortex';
-    LFP3 = struct2array(load('E:\BenA\SLAYTraining\SLAY1-3\10-27-16\cortex\LFP\LFP1000\LFP3.mat'));
-elseif animal == 'SLAY1-2'
-    exp_dir = 'E:\BenA\SLAYTraining\SLAY1-2\110416\cortex';
-    LFP3 = struct2array(load('E:\BenA\SLAYTraining\SLAY1-2\110416\cortex\LFP\LFP1000\LFPvoltage_ch3.mat'));
-elseif animal == 'SLAY1-1'
-    exp_dir = 'E:\BenA\SLAYTraining\SLAY1-1\120116\cortex';
-    LFP3 = struct2array(load('E:\BenA\SLAYTraining\SLAY1-1\120116\cortex\LFP\LFP1000\LFPvoltage_ch3.mat'));
-end
-
-cd(strcat(exp_dir, '\LFP\LFP1000\')); %change directory to experimental directory
-
-time = 1:length('LFPvoltage_ch.2.mat'); %time points
+time = 1:length('LFPvoltage_ch.2.mat'); %time points; 'LFPvoltage_ch.2.mat' can be any channel, as long as it exists, i.e. is not bad / has not been removed; purpose is to get number of indices in channel
 N = NaN([1,numchans]); %preallocate array N with length # of channels = 128
 
-%%Run if want run times
-%%need to have TimeCorr from program: RunTimes_BEN.m
-%%need to have LFPvecReorg matrix from program: ProbeReorganization_BEN.m
+%%need to have TimeCorr from program: Calculating-Run-Segments.m
+%%need to have LFPvecReorg matrix from program: Probe-Reorganization.m
 
 LFPvecReorgRun = nan(length(TimeCorr),2,1); %initialize 3-D matrix
 
 %%build array with NaN's before s(1,1), between e(n,2) and s(n+1,1), and
 %%after e(n,2)
 
-for n = 1:length(TimeCorr)
-    fdfd
-end
+%%THIS CODE NEEDS WORK.
 
 for n = 1:length(TimeCorr) %n = number of run segments
     startCorr = TimeCorr(n,1,:); %startCorr = start of each run segment
@@ -40,6 +25,8 @@ for n = 1:length(TimeCorr) %n = number of run segments
     length(n) = endCorr - startCorr; %length of run segment is endCorr - startCorr; length of n will change over loop (for each segment)
     LFPvecReorgRun(:,:,n) = [startCorr:endCorr];
 end
+
+%%THE CODE THAT FOLLOWS IS A FUNCTIONING ALTERNATIVE TO THE CODE ABOVE THAT NEEDS WORK. BOTH SHOULD PRODUCE THE SAME RESULT. THE CODE ABOVE WILL BE MORE EFFICIENT.
 
 tic;
 LFPvecReorgRun = LFPvecReorg;
@@ -59,19 +46,3 @@ if state == 'run'
     end
 end
 toc; %3000 seconds
-
-%%Run if want non-run times
-%%need to have LFPvecReorg matrix from program: ProbeReorganization_BEN.m
-tic;
-if state == 'nonrun'
-    for z = 1:64 %run through all channels
-        for i = 1:length(LFP3) %run through all LFP data points
-            for n = 1:length(TimeCorr)-1 %run through start/end-run matrix
-                if i > TimeCorr(n,1) && i < TimeCorr(n,2) %after start of run and before end of run = NaN
-                    LFPvector(z).LFPvoltage(:,i) = NaN;
-                end
-            end
-        end
-    end
-end
-toc; %300 seconds
