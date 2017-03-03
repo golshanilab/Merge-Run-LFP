c52c227582b5ef35718fc3ed63e319326aa7e5c9
bamendol <ben.amendolara@gmail.com>
2017-03-02 16:45:01 -0800

    Update matrix_LFP_run_nonrun_BEN.m
    - code works now
    - code works efficiently (3 for loops --> 1 for loop) (30000 seconds --> < 1 sec)

--- matrix_LFP_run_nonrun_BEN.m
+++ matrix_LFP_run_nonrun_BEN.m
@@ -14,35 +14,9 @@ N = NaN([1,numchans]); %preallocate array N with length # of channels = 128
 
 LFPvecReorgRun = nan(length(TimeCorr),2,1); %initialize 3-D matrix
 
-%%build array with NaN's before s(1,1), between e(n,2) and s(n+1,1), and
-%%after e(n,2)
-
-%%THIS CODE NEEDS WORK.
-
-for n = 1:length(TimeCorr) %n = number of run segments
-    startCorr = TimeCorr(n,1,:); %startCorr = start of each run segment
-    endCorr = TimeCorr(n,2,:); %endCorr = end of each run segment
-    length(n) = endCorr - startCorr; %length of run segment is endCorr - startCorr; length of n will change over loop (for each segment)
-    LFPvecReorgRun(:,:,n) = [startCorr:endCorr];
+LFPgeri = nan(size(LFPvecReorg),'single');
+for n = 1:size(TimeCorr,1)
+    startCorr = TimeCorr(n,1);
+    endCorr = TimeCorr(n,2);
+    LFPgeri(:,[startCorr:endCorr]) = LFPvecReorg(:,[startCorr:endCorr]);
 end
-
-%%THE CODE THAT FOLLOWS IS A FUNCTIONING ALTERNATIVE TO THE CODE ABOVE THAT NEEDS WORK. BOTH SHOULD PRODUCE THE SAME RESULT. THE CODE ABOVE WILL BE MORE EFFICIENT.
-
-tic;
-LFPvecReorgRun = LFPvecReorg;
-if state == 'run'
-    for z = 1:size(LFPvecReorgRun,1) %run through all channels
-        for i = 1:size(LFPvecReorg,2) %run through all LFP data points
-            for n = 1:length(TimeCorr)-1 %run through start/end-run matrix
-                if i < TimeCorr(1,1) %if before start of first run --> NaN
-                    LFPvecReorgRun(z,i) = NaN; %
-                elseif i > TimeCorr(n,2) && i < TimeCorr(n+1,1)
-                    LFPvecReorgRun(z,i) = NaN;
-                elseif i > TimeCorr(n+1,2)
-                    LFPvecReorgRun(z,i) = NaN;
-                end
-            end
-        end
-    end
-end
-toc; %3000 seconds
